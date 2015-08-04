baf4d8e4be0b73b04493f290c4f745625094bf11
Du <hzsydy@foxmail.com>
2015-08-04 23:18:06 +0800

    把接口补完了

--- Ccao-big-homework/Ccao-big-homework-core/MyWindow.cs
+++ Ccao-big-homework/Ccao-big-homework-core/MyWindow.cs
@@ -1,12 +1,30 @@
 ﻿//Du 2015.8
 //All rights reserved.
 
+using System;
+using System.Drawing;
 namespace Ccao_big_homework_core
 {
     public abstract class MyWindow
     {
-        public abstract void DrawRect(int left, int top, int height, int width);
-        public abstract void DrawCircle(int left, int top, int radius);
-        public abstract void DrawLine(int left1, int top1, int left2, int top2);
+        /// <summary>
+        /// 此处采用了bridge模式，将抽象部分Ccao_big_homework_core与实现部分Ccao_big_homework分离；
+        /// 由此我们进行了分工，由我负责抽象，而邵键准同学负责实现；
+        /// 采用bridge模式使得我们可以各自独立完成自己的部分而没有耦合。
+        /// 以下是接口。所有的函数名都和system.drawing中的一样，这是为了方便邵键准同学的实现。
+        /// </summary>
+        public abstract void DrawRect(Pen pen, int left, int top, int width, int height);
+        public abstract void DrawCircle(Pen pen, int left, int top, int radius);
+        public abstract void DrawLine(Pen pen, int left1, int top1, int left2, int top2);
+        public abstract void DrawClosedCurve(Pen pen, Point[] points);
+        public abstract void DrawEllipse(Pen pen, int left, int top, int width, int height);
+        public abstract void DrawString(String str, Font font, Brush brush, int left, int top);
+        public abstract void DrawPie(Pen pen, int left, int top, int width, int height, int startAngle, int sweepAngle);
+        public abstract void FillRect(Brush brush, int left, int top, int width, int height);
+        public abstract void FillPolygon(Brush brush, Point[] points);
+        public abstract void FillPie(Brush brush, int left, int top, int width, int height, int startAngle, int sweepAngle);
+        public abstract void FillEllipse(Brush brush, int left, int top, int width, int height);
+        public abstract void FillCircle(Brush brush, int left, int top, int radius);
+        public abstract void FillClosedCurve(Brush brush, Point[] points);
     }
 }
