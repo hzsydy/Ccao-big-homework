1d3630cf57a11f8ef05596528e841418c0e05527
Du <hzsydy@foxmail.com>
2015-08-05 11:50:30 +0800

    清一下垃圾

---
+++ delete.bat
@@ -0,0 +1,29 @@
+@echo on
+
+del /s /f /q *.o
+del /s /f /q *.a  
+del /s /f /q *.cache
+del /s /f /q *.pdb
+del /s /f /q *.pdbx
+del /s /f /q *.pe
+del /s /f /q *.exe.config
+del /s /f /q *.vshost.exe
+del /s /f /q *.vshost.exe.config
+del /s /f /q *.vshost.exe.manifest
+del /s /f /q *.sdf
+del /s /f /q *.tlog
+del /s /f /q *.log
+del /s /f /q *.lastbuildstate
+del /s /f /q *.obj
+del /s /f /q *.idb
+del /s /f /q *.suo
+del /s /f /q *.ilk
+del /s /f /q *.exe
+del /s /f /q *.dll.config
+del /s /f /q *.baml
+del /s /f /q *.g.*
+del /s /f /q TemporaryGeneratedFile*.*
+del /s /f /q *.FileListAbsolute.txt
+del /s /f /q build*.*
+
+pause
