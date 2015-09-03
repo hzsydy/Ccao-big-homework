32e96e79608ef785ae4942288b29b488a1ef379b
Du <hzsydy@foxmail.com>
2015-09-03 20:36:38 +0800

    Emgucv Added

---
+++ Ccao-big-homework/Ccao-big-homework-core/Draw_Mode/PictureMode.cs
@@ -0,0 +1,16 @@
+﻿//Du 2015.8
+//All rights reserved.
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Ccao_big_homework_core.Draw_Mode
+{
+    public sealed class PictureMode : DrawMode
+    {
+        public PictureMode(Pen p) : base() { }
+        public override void Fill(MyWindow w, GraphicsPath p)
+        {
+        }
+    }
+}
