baecbfbc3ece479c1252e8569b453ca1b364e82a
qyz55 <sjzwf501@126.com>
2015-09-06 14:54:06 +0800

    不知道动了什么

--- Ccao-big-homework/Ccao-big-homework/WorkWindow.xaml.cs
+++ Ccao-big-homework/Ccao-big-homework/WorkWindow.xaml.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Shapes;
 using System.Drawing;
 using Ccao_big_homework_core;
 using System.Drawing.Drawing2D;
@@ -40,7 +32,7 @@ namespace Ccao_big_homework
         }
         public void DrawPath(Pen pen, GraphicsPath path) 
         { 
-
+            
         }
         public void FillPath(Brush brush, GraphicsPath path) 
         {
