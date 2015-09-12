bb966d6147c61ac8e51bcaf6c55185274c067a21
Du <hzsydy@foxmail.com>
2015-09-12 14:48:42 +0800

    git push

--- Ccao-big-homework/Ccao-big-homework-core-wpf/DrawingUIElement.cs
+++ Ccao-big-homework/Ccao-big-homework-core-wpf/DrawingUIElement.cs
@@ -9,12 +9,8 @@ namespace Ccao_big_homework_core_wpf
     public class DrawingUIElement : UIElement
     {
         public Drawing drawing { get; set; }
-<<<<<<< HEAD
         public DrawingUIElement(Drawing d) : base() { drawing = d; }
         public DrawingUIElement() : base() { }
-=======
-        //public DrawingUIElement(Drawing d): base() { drawing = d; }
->>>>>>> origin/Pierre
         protected override void OnRender(DrawingContext drawingContext)
         {
             drawingContext.DrawDrawing(drawing);
