1888bf4f7197ccba1a4c7b8e7cfbf1bc1866e39c
Du <hzsydy@foxmail.com>
2015-09-12 14:30:17 +0800

    紧急修bug

--- Ccao-big-homework/Ccao-big-homework-core-wpf/DrawingUIElement.cs
+++ Ccao-big-homework/Ccao-big-homework-core-wpf/DrawingUIElement.cs
@@ -6,10 +6,11 @@ using System.Windows.Media;
 
 namespace Ccao_big_homework_core_wpf
 {
-    class DrawingUIElement : UIElement
+    public class DrawingUIElement : UIElement
     {
         public Drawing drawing { get; set; }
-        public DrawingUIElement(Drawing d): base() { drawing = d; }
+        public DrawingUIElement(Drawing d) : base() { drawing = d; }
+        public DrawingUIElement() : base() { }
         protected override void OnRender(DrawingContext drawingContext)
         {
             drawingContext.DrawDrawing(drawing);
